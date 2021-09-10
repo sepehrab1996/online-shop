13e640fb5b2bc8f52874aaf9e80b40d175cffc96
sepehrab1996@gmail.com <sepehrab1996@gmail.com>
2021-09-10 17:11:42 +0430

    some change on online_shop\urls.py

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('customers/', include('customers.urls')),
-    path('products/', include('products.urls')),
-    path('orders/', include('orders.urls')),
+    # path('customers/', include('customers.urls')),
+    # path('products/', include('products.urls')),
+    # path('orders/', include('orders.urls')),
 ]
