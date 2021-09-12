1a4c6ce11d3b0d92ece4a5db02e384bf9d29c227
sepehrab1996@gmail.com <sepehrab1996@gmail.com>
2021-09-13 03:24:52 +0430

    adding ShopUserImage model to customer\models.py and use MPTTModel for category

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Product(models.Model):
@@ -7,9 +8,17 @@ class Product(models.Model):
     inventory = models.PositiveIntegerField(default=0)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(max_length=200)
-    image = models.ImageField(upload_to='product_image')
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_image', default=None, null=True)
+
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=50, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
