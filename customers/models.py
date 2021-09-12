21415560f02b28d7b568c88cc4c9a4c04dff64fd
sepehrab1996@gmail.com <sepehrab1996@gmail.com>
2021-09-12 21:54:56 +0430

    adding ShopUser model to customers/models.py

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from orders.models import DiscountCode
 
-# Create your models here.
+
+class ShopUser(User):
+    identity_code = models.PositiveBigIntegerField(unique=True)
+    phone_number = models.TextField(max_length=20, unique=True)
+    profile_image = models.ImageField(upload_to='profile_image', default=None, null=True)
+
+
