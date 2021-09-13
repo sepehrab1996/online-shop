9b930047d2307181bf2c6a09e3b3ba76ae37ec6b
sepehrab1996@gmail.com <sepehrab1996@gmail.com>
2021-09-13 10:36:21 +0430

    adding mptt.admin to products\admin.py and register category

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Category, Product
+from mptt.admin import MPTTModelAdmin, DraggableMPTTAdmin
 
-admin.site.register(Category)
+admin.site.register(Category, DraggableMPTTAdmin)
 admin.site.register(Product)
