7c37e44607869d9250fce9780a9e5feb2cfef3ad
sepehrab1996@gmail.com <sepehrab1996@gmail.com>
2021-09-13 11:17:22 +0430

    register ProductImage model in products\admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, ProductImage
 from mptt.admin import MPTTModelAdmin, DraggableMPTTAdmin
 
 admin.site.register(Category, DraggableMPTTAdmin)
 admin.site.register(Product)
+admin.site.register(ProductImage)
