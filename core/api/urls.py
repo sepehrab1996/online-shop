427d5c90bd8415f6eabdc9c57295ed9996d651c8
sepehrab1996@gmail.com <sepehrab1996@gmail.com>
2021-10-01 12:04:14 +0330

    change on customers path in customers\api\urls.py

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('users/', include('customers.api.urls')),
+    path('customers/', include('customers.api.urls')),
     path('products/', include('products.api.urls')),
     path('orders/', include('orders.api.urls')),
 ]
