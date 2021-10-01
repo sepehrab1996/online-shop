5f0398e3e7c8d6d9b88adb8760d0af784c49edf1
sepehrab1996@gmail.com <sepehrab1996@gmail.com>
2021-10-01 11:44:49 +0330

    adding ShowCustomerRegisterForm view to customers app for render the register form

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from .forms import CustomerRegisterForm
 
-# Create your views here.
+
+class ShowCustomerRegisterForm(View):
+    def get(self, request):
+        form = CustomerRegisterForm()
+        return render(request, 'customers/customer_register.html', context={
+            'form': form
+        })
