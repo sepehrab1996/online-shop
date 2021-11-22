8021bfea404c9dcc4310ebc02f154eff52fd90bb
sepeh <sepehrab1996@gmail.com>
2021-11-22 19:06:12 +0330

    some change on tests.py

--- customer/tests.py
+++ customer/tests.py
@@ -6,8 +6,9 @@ from customer.models import Customer
 
 class TestLoginView(TestCase):
     def setUp(self):
-        customer=Customer.objects.create(username='test_user', email='test@gmail.com',)
+        customer = Customer.objects.create(username='test_user', email='test@gmail.com', )
         customer.set_password('secret')
+
     def test_login(self):
-        response = self.client.post('/en/customer/login/', {'username': 'morteza', 'password': 'secret'})
+        response = self.client.post('/en/customer/login/', {'username': 'sepehrab1996', 'password': 'secret'})
         self.assertEqual(response.status_code, 200)
