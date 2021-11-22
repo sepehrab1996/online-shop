af9f7196ed4dd3e62651270198bff420f332ceee
sepeh <sepehrab1996@gmail.com>
2021-11-22 19:00:01 +0330

    remove advertising model from admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, Attribute, Category, ProductImage,Advertising
+from .models import Product, Attribute, Category, ProductImage
 
 admin.site.register(Attribute)
 admin.site.register(Category)
 admin.site.register(ProductImage)
 admin.site.register(Product)
-admin.site.register(Advertising)
