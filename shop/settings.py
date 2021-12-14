7e1fe17d5ed433d58348a7a0e3f10e9c630cb291
sepehrab1996 <sepehrab1996@gmail.com>
2021-12-15 00:25:42 +0330

    some change on database settings

--- shop/settings.py
+++ shop/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'shop',
-        'USER': 'postgres',
+        'USER': 'sepehrab1996',
         'PASSWORD': '09382282100',
         'HOST': '127.0.0.1',
         'PORT': '5432',
